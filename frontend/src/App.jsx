e9a4ea4bc7735df6aa9b4f19537f42f55d77f286
MaivMinh <maivanminh.se@gmail.com>
2025-03-21 21:10:08 +0700

    Complete importing students from a JSON file and exporting students to CSV and JSON files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import StudentTable from './components/StudentTable';
 import EditStudent from './components/EditStudent';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {StudentProvider} from './contexts/StudentContext';
+import Import from './components/Import';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<StudentTable />} />
+          <Route path='/import' element={<Import />} />
           <Route path="/edit/:id" element={<EditStudent />} />
         </Routes>
       </Router>
